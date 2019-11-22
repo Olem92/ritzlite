5b53dd85482b977505af224a119b473013820b60
Danny <Dannyblackbourn10@gmail.com>
2019-11-22 18:54:14 +0000

    fix woodcutting plugin

--- runelite-client/src/main/java/net/runelite/client/plugins/woodcutting/TreeRespawn.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/woodcutting/TreeRespawn.java
@@ -28,19 +28,14 @@ package net.runelite.client.plugins.woodcutting;
 import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldPoint;
 
 @AllArgsConstructor
 @Getter
 class TreeRespawn
 {
 	private final Tree tree;
-	private final LocalPoint location;
+	private final WorldPoint worldPoint;
 	private final Instant startTime;
 	private final int respawnTime;
-
-	boolean isExpired()
-	{
-		return Instant.now().isAfter(startTime.plusMillis(respawnTime));
-	}
 }
