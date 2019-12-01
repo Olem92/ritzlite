260f3ee926f6213497e2dace6b62f37da149fd5c
Danny <Dannyblackbourn10@gmail.com>
2019-12-01 16:23:34 +0000

    Updated priority of the plugin
    This moves the button on the side bar further up

--- runelite-client/src/main/java/net/runelite/client/plugins/profiles/ProfilesPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/profiles/ProfilesPlugin.java
@@ -39,7 +39,7 @@ public class ProfilesPlugin extends Plugin {
         navButton = NavigationButton.builder()
                 .tooltip("Profiles")
                 .icon(icon)
-                .priority(8)
+                .priority(2)
                 .panel(panel)
                 .build();
 
